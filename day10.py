8602ec888919cc02093bc1373d1ed0f083635bd6
Brian Ciccolo <brian@passingfocus.net>
2021-12-10 10:11:32 -0500

    Solved part 1

---
+++ day10.py
@@ -0,0 +1,40 @@
+FILE_NAME = 'day10.dat'
+
+
+def part1():
+    global incomplete_entries
+
+    # Step 1: Find the corrupt tokens
+    file = open(FILE_NAME, 'r')
+    lines = file.readlines()
+
+    jolts = [int(x) for x in lines]
+
+    jolts.sort()
+
+    print(jolts)
+
+    singleCount = 0
+    tripleCount = 1 # Count the built-in adapter
+
+    lastJolt = 0
+    for jolt in jolts:
+        if jolt - lastJolt == 1:
+            singleCount = singleCount + 1
+        elif jolt - lastJolt == 3:
+            tripleCount = tripleCount + 1
+
+        lastJolt = jolt
+
+    product = singleCount * tripleCount
+
+    print('Part 1: ' + str(product))
+
+
+def part2():
+
+    print('Part 2: ' + str(0))
+
+
+part1()
+part2()
